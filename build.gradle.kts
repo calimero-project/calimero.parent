ed2202eff0e7d543e31b51f95db08397c8becce8
bmalinowsky <bmalinowsky@github.com>
2024-10-31 16:11:54 +0100

    Convert to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+	`java-library`
+	id("com.github.ben-manes.versions") version "0.51.0"
+	id("com.github.spotbugs") version "6.0.25"
+	`maven-publish`
+}
+
+val desc = "Calimero composite build"
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
+	}
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+	dependsOn(gradle.includedBuilds.map { it.task(":dependencyUpdates") })
+}
+
+tasks.named<DefaultTask>("clean") {
+	dependsOn(gradle.includedBuilds.map { it.task(":clean") })
+}
+
+tasks.named<DefaultTask>("assemble") {
+	dependsOn(gradle.includedBuilds.map { it.task(":assemble") })
+}
+
+tasks.named<DefaultTask>("publishToMavenLocal") {
+	dependsOn(gradle.includedBuilds.map { it.task(":publishToMavenLocal") })
+}
+
+tasks.named<Wrapper>("wrapper") {
+	gradleVersion = "8.11-rc-1"
+    dependsOn(gradle.includedBuilds.map { it.task(":wrapper") })
+}
