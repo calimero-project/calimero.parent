ef97e5bb980211f3b352f8543ddd41110a142f63
bmalinowsky <bmalinowsky@github.com>
2026-05-02 16:19:07 +0200

    Upgrade to spotbugs 6.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
 	`java-library`
 	id("com.github.ben-manes.versions") version "0.54.0"
-	id("com.github.spotbugs") version "6.5.0"
+	id("com.github.spotbugs") version "6.5.1"
 	`maven-publish`
 }
 
