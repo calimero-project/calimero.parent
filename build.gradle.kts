5d8eb3abb0c09b8fadc499ec4c904d7e6e82fedc
bmalinowsky <bmalinowsky@github.com>
2026-09-13 12:48:23 +0200

    Upgrade to versions 0.62.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
 	`java-library`
-	id("com.github.ben-manes.versions") version "0.54.0"
+	id("com.github.ben-manes.versions") version "0.62.0"
 	id("com.github.spotbugs") version "6.5.6"
 	`maven-publish`
 }
